940d8e68469a1366f5ba1d84751501208db40bd8
imdivyang <divyangparekh0@gmail.com>
2025-06-27 16:08:31 +0100

    chnages made in style

--- App.tsx
+++ App.tsx
@@ -6,14 +6,8 @@
 //    - [optional] Use a **virtualized list** (e.g. `FlatList`) for performance if the dataset is large
 
 import React, { useMemo } from "react";
-import {
-  FlatList,
-  SafeAreaView,
-  SectionList,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { FlatList, SafeAreaView, SectionList, Text, View } from "react-native";
+import styles from "./App.style";
 import transactionData from "./data/transactions.json";
 
 interface transactionData {
@@ -124,52 +118,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  safeAreaContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginVertical: 10,
-  },
-  contentContainerStyle: {},
-  transactionContainer: {
-    borderWidth: 1,
-    borderColor: "#eb4034",
-    marginBottom: 20,
-    padding: 10,
-    borderRadius: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  details: {
-    flex: 1,
-    alignItems: "flex-end",
-  },
-  dateLabel: {
-    fontSize: 16,
-  },
-  item: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  sectionTotal: {
-    marginTop: 10,
-    fontWeight: "bold",
-    textAlign: "right",
-    borderBottomWidth: 1,
-    borderBottomColor: "#eb4034",
-  },
-  overTotalInfo: {
-    fontSize: 16,
-    fontWeight: "bold",
-    marginTop: 20,
-    textAlign: "right",
-  },
-});
